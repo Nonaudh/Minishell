c9758de253d8218ca9abe156ab412ee6319f4017
Arnaud Huge <ahuge@2A3.42angouleme.fr>
2024-05-28 13:29:28 +0200

    ok

--- cmd
+++ cmd
@@ -1,1 +1,6 @@
-test> | "test"||''''''
+echo $?
+ls -la > out
+echo hi > out < cat | cat > out2
+echo "salut | >" | cat > 'o''u''t'
+echo hi | echo >./outfiles/outfile01 bye >./test_files/invalid_permission
+echo '> >> < * ? [ ] | ; [ ] || && ( ) & # $  <<'
--- src/main.c
+++ src/main.c
@@ -11,14 +11,14 @@ int	is_a_token(char c)
 	return (0);
 }
 
-int	lexing(char *str)
+int	count_argc(char *str)
 {
 	int i = 0;
 	int count = 0;
 
 	while (str[i])
 	{
-		if (is_a_token(str[i]) || (str[i] != 32 && (str[i + 1] == 32 || !str[i + 1] || is_a_token(str[i + 1]))))
+		if (is_a_token(str[i]) || (str[i] > 32 && (str[i + 1] <= 32 || is_a_token(str[i + 1]))))
 			count++;
 		i++;
 		if (str[i] == 34)
@@ -37,6 +37,18 @@ int	lexing(char *str)
 	return (count);
 }
 
+int	minishell(char *line)
+{
+	int	argc;
+	char **lex;
+
+	argc = count_argc(line);
+	printf("argc; %d\n", argc);
+
+
+	return (0);
+}
+
 int main(void)
 {
 	char *line;
@@ -47,10 +59,18 @@ int main(void)
 	if (fd == -1)
 		return (1);
 	line = get_next_line(fd);
-
-	i = lexing(line);
-	printf("i; %d\n", i);
-
+	if (line)
+		line[ft_strlen(line) - 1] = 0;
+	while (line)
+	{
+		printf("%s\n", line);
+		minishell(line);
+		printf("\n");
+		free(line);
+		line = get_next_line(fd);
+		if (line)
+			line[ft_strlen(line) - 1] = 0;
+	}
 	free(line);
 	return (0);
 }
