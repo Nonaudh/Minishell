ff8b7633e51e6ca0a6111610bcd41d95636f4d25
Nonaudh <nonaudh@gmail.com>
2024-05-28 22:36:12 +0200

    ok

--- cmd
+++ cmd
@@ -1,7 +1,7 @@
 echo $?
 ls -la > out
 echo hi > out < cat | cat > out2
-echo "salut | >""test" | cat > 'o''u''t'
+echo    "salut | >""test"   |  "" cat >   'o''u''t'         |
 echo hi | echo >./outfiles/outfile01 bye >./test_files/invalid_permission
 echo '> >> < * ? [ ] | ; [ ] || && ( ) & # $  <<'
 """"""""c"l"ydksi""""""""""""
--- src/main.c
+++ src/main.c
@@ -11,6 +11,45 @@ int	is_a_token(char c)
 	return (0);
 }
 
+char	*create_token(t_token token)
+{
+	char	*str;
+	str = malloc(sizeof(str) * 2);
+	str[0] = token;
+	str[1] = 0;
+	return (str);
+}
+int	print_token(t_token token)
+{
+	if (token == PIPE)
+	{
+		printf("PIPE\n");
+		return (1);
+	}
+	if (token == GREAT)\
+	{
+		printf("GREAT\n");
+		return (1);
+	}
+	if (token == LESS)
+	{
+		printf("LESS\n");
+		return (1);
+	}
+	return (0);	
+}
+
+char	*find_token(char c)
+{
+	if (c == '|')
+		return (create_token(PIPE));
+	if (c == '>')
+		return (create_token(GREAT));
+	if (c == '<')
+		return (create_token(LESS));
+	return (0);
+}
+
 int	count_argc(char *str)
 {
 	int i = 0;
@@ -27,7 +66,10 @@ int	count_argc(char *str)
 			while (str[i] && str[i] != 34)
 				i++;
 			if (!str[i])
+			{
 				printf("Error quote\n");
+				return (-1);
+			}			
 		}
 		if (str[i] == 39)
 		{
@@ -35,15 +77,51 @@ int	count_argc(char *str)
 			while (str[i] && str[i] != 39)
 				i++;
 			if (!str[i])
+			{
 				printf("Error quote\n");
+				return (-1);
+			}
 		}
 	}
 	return (count);
 }
 
+
 char	*next_argv(char *line, int *x)
 {
-	while (line[*x])
+	int	i = *x;
+	char	*str;
+	int	start;
+	int	end;
+
+	while (line[i] <= 32)
+		i++;
+	if (is_a_token(line[i]))
+	{
+		str = find_token(line[i]);
+		*x = i + 1;
+		return (str);
+	}
+	start = i;
+	while (line[i] > 32 && !is_a_token(line[i]))
+	{
+		if (line[i] == 34)
+		{
+			i++;
+			while (line[i] != 34)
+				i++;
+		}
+		if (line[i] == 39)
+		{
+			i++;
+			while (line[i] != 39)
+				i++;
+		}
+		i++;
+	}
+	str = ft_substr(line, start, i - start);
+	*x = i;
+	return (str);
 }
 
 char	**fill_lex(char *line, int argc)
@@ -56,8 +134,13 @@ char	**fill_lex(char *line, int argc)
 	while (i < argc)
 	{
 		lex[i] = next_argv(line, &x);
+		if (print_token(lex[i][0]))
+			;
+		else
+			printf("%s\n", lex[i]);
 		i++;
-	}	
+	}
+	lex[i] = NULL;
 	return (lex);
 }
 
@@ -68,6 +151,8 @@ char	**lexing(char *line)
 
 	argc = count_argc(line);
 	printf("argc; %d\n", argc);
+	if (argc == -1)
+		return (NULL);
 	lex = fill_lex(line, argc);
 
 	return (NULL);
