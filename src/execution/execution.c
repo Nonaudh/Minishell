75e7b52b4158f891160baf72e716c2af0e870ba6
Arnaud Huge <ahuge@2D3.42angouleme.fr>
2024-06-20 16:47:13 +0200

    ok

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -1,11 +1,31 @@
 #include "../../inc/minishell.h"
 
-int	exec_cmd(t_commands *cmd, t_commands *root, int i)
+int	close_all_fd_except(t_commands *root, int i, int size)
 {
-	close_all_fd_except(root, i);
+	int	x;
+
+	x = 0;
+	while (x < size)
+	{
+		if (x != i)
+		{
+			if (close(root->fd_in))
+				printf("Error close fd_in\n");
+			if (close(root->fd_out))
+				printf("Error close fd_out\n");			
+		}
+		x++;
+	}
+	return (0);
 }
 
-int execute_in_child_process(t_commands *cmd, t_commands *root, int i)
+int	exec_cmd(t_commands *cmd, t_commands *root, int i, int size)
+{
+	close_all_fd_except(root, i, size);
+	
+}
+
+int execute_in_child_process(t_commands *cmd, t_commands *root, int i, int size)
 {
 	int fork_pid;
 	int exit_status;
@@ -15,17 +35,17 @@ int execute_in_child_process(t_commands *cmd, t_commands *root, int i)
 		exit (EXIT_FAILURE);
 	if (fork_pid == 0)
 	{
-		exit_status = exec_cmd(cmd, root, i);
+		exit_status = exec_cmd(cmd, root, i, size);
 		return (exit_status);
 	}
 	return (0);
 }
 
-int execute_command(t_commands *cmd, t_commands *root, int i)
+int execute_command(t_commands *cmd, t_commands *root, int i, int size)
 {
 	int exit_status;
 
-	exit_status = execute_in_child_process(cmd, root, i);
+	exit_status = execute_in_child_process(cmd, root, i, size);
 }
 
 int execution(t_commands *cmd, int size)
@@ -35,7 +55,7 @@ int execution(t_commands *cmd, int size)
 
 	while(i < size)
 	{
-		exit_status = execute_command(&cmd[i], cmd, i);
+		exit_status = execute_command(&cmd[i], cmd, i, size);
 		i++;
 	}
 	return (exit_status);
